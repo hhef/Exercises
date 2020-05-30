0478b4c5cc74768ede9462ac9e1dfb40fad8c84f
Krzysztof Suder <ksuder7@gmail.com>
2020-05-30 19:06:31 +0200

    check if letter was already used and end game

---
+++ README.md
@@ -0,0 +1,6 @@
+# Hangman game
+> This is simple hangman game for one player
+
+## General info
+You have 6 tries to guess the word. Word is randomly chosen from words.txt file.
+Each turn program will print letters you guessed, letters that match and how many tries you got
--- hangman.py
+++ hangman.py
@@ -19,3 +19,15 @@ while tries >= 1:
     for i in range(len(word)):
         if letter == word[i]:
             word_list[i] = letter
+
+    if letter in player_letters:
+        print("You already tried this letter, please pick another one")
+    elif letter not in word:
+        tries -= 1
+
+    if "_" not in word_list or letter == word:
+        print(f"cg, you won, the word was {word}")
+        break
+
+    player_letters.append(letter)
+    print(*word_list, sep=" ")
