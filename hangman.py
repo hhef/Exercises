1d8f9ec52b90e6c9aa080b876453aeda210706eb
Krzysztof Suder <ksuder7@gmail.com>
2020-05-30 18:55:35 +0200

    add few information prints for player

--- hangman.py
+++ hangman.py
@@ -9,3 +9,9 @@ word_list = ["_" for i in range(len(word))]
 print("Welcome to Hangman, here is your word:")
 print(*word_list, sep=" ")
 tries = 6
+
+while tries >= 1:
+    print("\nYour guessed letters: ", end="")
+    print(*player_letters, sep=", ")
+    print(f"You have {tries} more guesses")
+    letter = input("Please guess a letter: ").upper()
