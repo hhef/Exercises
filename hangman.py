5c5697adeca4889ab1d62ad627c16f258a92c547
Krzysztof Suder <ksuder7@gmail.com>
2020-05-30 18:52:23 +0200

    add welcome and print _ for each letter

--- hangman.py
+++ hangman.py
@@ -3,3 +3,9 @@ import random
 with open('words.txt', 'r') as file:
     words = file.readlines()
     word = random.choice(words).strip()
+
+player_letters = []
+word_list = ["_" for i in range(len(word))]
+print("Welcome to Hangman, here is your word:")
+print(*word_list, sep=" ")
+tries = 6
