a6b118eb92e5902e8a79a019de02f3acae5e6458
Krzysztof Suder <ksuder7@gmail.com>
2020-05-31 11:16:45 +0200

    checking if numer is number and append to list

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 import random
 
 random_number = [random.randint(1, 9) for i in range(4)]
+
+while True:
+    input_numbers = input("Guess a number: ")
+    user_numbers = []
+    if input_numbers not in "1234567890":
+        continue
+    else:
+        user_numbers.append(int(input_numbers))
+    cows = 0
+    bulls = 0
+
