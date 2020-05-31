a0be5fa97fcfccbda17385669b52b1358f0ff2e8
Krzysztof Suder <ksuder7@gmail.com>
2020-05-31 11:30:28 +0200

    check if win and printing cows, bulls and last guess

--- app.py
+++ app.py
@@ -16,3 +16,10 @@ while True:
             cows += 1
         elif user_numbers[index] in random_number:
             bulls += 1
+
+    if cows == 4:
+        print("cg dudz")
+        break
+
+    print(f"{cows} cows, {bulls} bulls")
+    print(f"Last guess: {input_numbers}")
