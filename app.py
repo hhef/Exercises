41593b5575e7f1255c97d9421776f56a748bb7fd
Krzysztof Suder <ksuder7@gmail.com>
2020-05-31 11:28:26 +0200

    check how many cows and bulls

--- app.py
+++ app.py
@@ -3,12 +3,16 @@ import random
 random_number = [random.randint(1, 9) for i in range(4)]
 
 while True:
-    input_numbers = input("Guess a number: ")
+    input_numbers = input("Guess a 4 digit number: ")
     user_numbers = []
-    if input_numbers not in "1234567890":
-        continue
-    else:
-        user_numbers.append(int(input_numbers))
+    for i in input_numbers:
+        if i not in "1234567890":
+            continue
+        user_numbers.append(int(i))
     cows = 0
     bulls = 0
-
+    for index in range(4):
+        if user_numbers[index] == random_number[index]:
+            cows += 1
+        elif user_numbers[index] in random_number:
+            bulls += 1
